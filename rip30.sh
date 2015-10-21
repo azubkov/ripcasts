dc5654294d77da92b611c25735db0ebe874cfecd
Artem Zubkov <artem.zubkoff@gmail.com>
2015-10-21 21:10:11 +0200

    improvements

--- rip30.sh
+++ rip30.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 ##record 30 songs from internet radio
-
+TRACK_COUNT=6
 DIRNAME=cast_`date +%m-%d-%y`
-FILENAME=psychedelik_amb_%m-%d-%y-%H-%M-%S
+FILENAME_PREFIX=psychedelik_amb_
 URL=http://195.154.86.144:8002/relay.mp3?icy=http
-mkdir $DIRNAME
-cd $DIRNAME
+
+function printUsage {
+    echo "--------------"    
+    echo "usage:"    
+    echo "run30.sh <trackCount>"    
+    echo "--------------"    
+}
+
+function initTrackCount {
+  if [ -z "$1" ]
+  then  
+  ##alredy init by default value
+  TRACK_COUNT=$TRACK_COUNT;
+  else 
+  TRACK_COUNT=$1
+  fi
+  echo "TRACK_COUNT is $TRACK_COUNT"
+}
 
 function recordTrack {
-    echo "recordTrack called FILENAME:$FILENAME URL: $URL" 
-    icecream --name=$FILENAME --stop=90mb $URL
+    echo "recordTrack called FILENAME_PREFIX:$FILENAME_PREFIX URL: $URL" 
+    icecream --name=$FILENAME_PREFIX%m-%d-%y-%H-%M-%S --stop=90mb $URL
 }
 
 function filterSmallFiles {
@@ -17,7 +33,7 @@ function filterSmallFiles {
 }
 
 function cutPrefixAds {
-  FILES=`find . -maxdepth 1  -type f  -name "*.mp3" -printf '%f\n'`
+  FILES=`find . -maxdepth 1  -type f  -name "$FILENAME_PREFIX*.mp3" -printf '%f\n'`
   for i in $FILES
   do 
     file="`echo -n "$i" `"
@@ -33,8 +49,15 @@ function cutPrefixAds {
 }
 
 
-COUNTER=0
-while [  $COUNTER -lt 6 ]; do
+printUsage
+initTrackCount $1
+
+
+mkdir $DIRNAME
+cd $DIRNAME
+
+COUNTER=0;
+while [  $COUNTER -lt $TRACK_COUNT ]; do
     echo The counter is $COUNTER
     let COUNTER=COUNTER+1 
     recordTrack
